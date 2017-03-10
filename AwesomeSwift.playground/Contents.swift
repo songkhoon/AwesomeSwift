908b33a6419003ea4a6cc69e2edcf85827afd9d2
Jeff Lim <t_khoon@hotmail.com>
2017-03-11 06:45:14 +0800

    set view size on playground

--- AwesomeSwift.playground/Contents.swift
+++ AwesomeSwift.playground/Contents.swift
@@ -7,5 +7,6 @@ import PlaygroundSupport
 var str = "Hello, playground"
 PlaygroundPage.current.needsIndefiniteExecution = true
 let controller = LandingViewController()
+controller.view.frame.size = CGSize(width: 375, height: 667)
 let navController = UINavigationController(rootViewController: controller)
 PlaygroundPage.current.liveView = navController
