30834c00ee523ecb26c7c5e7ee8eed522bb18ec8
jeff <jeff.lim@compass-interactive.com>
2017-03-13 15:59:06 +0800

    add submenu

--- AwesomeSwift.playground/Sources/Landing/LandingViewController.swift
+++ AwesomeSwift.playground/Sources/Landing/LandingViewController.swift
@@ -12,35 +12,22 @@ public class LandingViewController: UIViewController {
     
     let landingCellId = String(describing: LandingCell.self)
     var landingButtons = [UIButton]()
-    
-    let collectionView:UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.estimatedItemSize = CGSize(width: 200, height: 50)
-        let view = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
-        view.backgroundColor = .orange
-        view.contentInset = UIEdgeInsetsMake(30, 0, 30, 0)
-        return view
-    }()
+    public let landingMenus:[String:[String:AnyClass]] = [
+        "Animation":[:],
+        "Calendar":[
+            "CalendarKit":ExampleController.self,
+            "CalendarView":CalendarViewController.self,
+            "Koyomi":KoyomiViewController.self
+        ]
 
+    ]
+    
     override public func viewDidLoad() {
         super.viewDidLoad()
         automaticallyAdjustsScrollViewInsets = false
         view.backgroundColor = .white
         navigationItem.title = "Landing"
         
-        let calendarKitButton = createButton("CalendarKit")
-        calendarKitButton.addTarget(self, action: #selector(handleCalendarKit), for: .touchUpInside)
-        let calendarViewButton = createButton("CalendarView")
-        calendarViewButton.addTarget(self, action: #selector(handleCalendarView), for: .touchUpInside)
-        let koyomiButton = createButton("Koyomi")
-        koyomiButton.addTarget(self, action: #selector(handleKoyomi), for: .touchUpInside)
-        
-        landingButtons = [
-            calendarKitButton,
-            calendarViewButton,
-            koyomiButton
-        ]
-
         setupLayout()
     }
 
@@ -54,64 +41,41 @@ public class LandingViewController: UIViewController {
     }
     
     private func setupLayout() {
-        collectionView.register(LandingCell.self, forCellWithReuseIdentifier: landingCellId)
-        collectionView.dataSource = self
+        let layout = UICollectionViewFlowLayout()
+        layout.estimatedItemSize = CGSize(width: 200, height: 50)
+        let menuCollection = MenuCollectionViewController(collectionViewLayout: layout)
+        addChildViewController(menuCollection)
+        view.addSubview(menuCollection.view)
+        menuCollection.view.translatesAutoresizingMaskIntoConstraints = false
+        menuCollection.view.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 0).isActive = true
+        menuCollection.view.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor).isActive = true
+        menuCollection.view.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        menuCollection.view.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         
-        let layoutGuide = UILayoutGuide()
-        view.addLayoutGuide(layoutGuide)
-        layoutGuide.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor).isActive = true
-        layoutGuide.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor).isActive = true
-        layoutGuide.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        layoutGuide.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        menuCollection.didMove(toParentViewController: self)
         
-        view.addSubview(collectionView)
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        collectionView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor).isActive = true
-        collectionView.widthAnchor.constraint(equalTo: layoutGuide.widthAnchor, multiplier: 1).isActive = true
-        collectionView.heightAnchor.constraint(equalTo: layoutGuide.heightAnchor, multiplier: 1).isActive = true
-    }
-
-    private func createButton(_ label:String) -> UIButton {
-        let button = UIButton()
-        button.layer.cornerRadius = 10.0
-        button.setTitle(label, for: .normal)
-        button.backgroundColor = .blue
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        return button
+        var menus = [UIButton:AnyClass]()
+        var buttons = [UIButton]()
+        for item in landingMenus {
+            let button = createButton(item.key)
+            button.addTarget(self, action: #selector(handleSubmenu(_:)), for: .touchUpInside)
+            buttons.append(button)
+            menus[button] = SubmenuViewController.self
+        }
+        menuCollection.buttons = buttons
+        
     }
     
     // MARK:- Handlers
-    func handleCalendarKit() {
-        let navController = UINavigationController(rootViewController: ExampleController())
-        navigationController?.present(navController, animated: true, completion: nil)
+    func handleSubmenu(_ sender:UIButton) {
+        let controller = SubmenuViewController()
+        if let title = sender.title(for: .normal) {
+            controller.subMenus = landingMenus[title]
+            controller.navigationItem.title = title
+            let navController = UINavigationController(rootViewController: controller)
+            navController.title = title
+            present(navController, animated: true, completion: nil)
+        }
     }
     
-    func handleCalendarView() {
-        let navController = UINavigationController(rootViewController: CalendarViewController())
-        navigationController?.present(navController, animated: true, completion: nil)
-    }
-    
-    func handleKoyomi() {
-        let navController = UINavigationController(rootViewController: KoyomiViewController())
-        navigationController?.present(navController, animated: true, completion: nil)
-    }
-
-
 }
-
-extension LandingViewController:UICollectionViewDataSource {
-    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return landingButtons.count
-    }
-    
-    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: landingCellId, for: indexPath) as! LandingCell
-        cell.addSubview(landingButtons[indexPath.row])
-        return cell
-    }
-}
-
-
